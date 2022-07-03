76d98ae392d64ad31b1a9ff111ebcfb50a81388e
John Chavez <johnchavez@Johns-MBP-2.lan>
2022-07-03 15:37:45 -0400

    adding footer. Ticker is not working. Commenting out of Footer.js

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,5 +1,7 @@
 import React, {useRef, useState } from 'react';
 import "./Video.css";
+import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
 function Video() {
   const [playing, setPlaying] = useState(false);  
@@ -32,7 +34,8 @@ function Video() {
         src="https://v16m-default.tiktokcdn-us.com/b669c69aafa15c785220c207c54c26b2/62c22c15/video/tos/useast5/tos-useast5-ve-0068c002-tx/3863576d57624689a1fc5c9388365a91/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2880&bt=1440&cs=0&ds=3&ft=eXWR6H-qMyq8ZvdZ6we2NmE~fl7Gb&mime_type=video_mp4&qs=0&rc=NTc4aGk7OjVpNmRoNmgzOkBpM3FlbGY6Zms7PDMzZzczNEBjMTFiNTEuNi0xLzVeXzJhYSMxNGNpcjRnbWhgLS1kMS9zcw%3D%3D&l=20220703175345010002045007735002021084F99F6">
 
         </video>
-        {/*Video Footer*/}
+        <VideoFooter/>
+        <VideoSidebar/>
         {/*Video sidebar*/}
     </div>
   )
--- tiktok/src/VideoFooter.js
+++ tiktok/src/VideoFooter.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './VideoFooter.css';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+// import Ticker from 'react-ticker';
 
 function VideoFooter() {
-  return (
-    <div className='videoFooter'></div>
-  )
+  return ( 
+        <div className='videoFooter'>
+            <div className='videoFooter__text'>
+                <h3>@eljohncena</h3>
+                <div className='videoFooter__ticker'>
+                    <MusicNoteIcon className='videoFooter__icon'/>
+
+                    {/* <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>I am a song</p>
+                            </>
+                        )}
+                    </Ticker> */}
+                </div>
+            </div>
+            <img className='videoFooter__record' src='https://static.thenounproject.com/png/934821-200.png'/>
+        </div>
+    );
 }
 
-export default VideoFooter
+export default VideoFooter;
