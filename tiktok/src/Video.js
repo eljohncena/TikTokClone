5d2ab1655dca0464d9217c1bc5a4189be0f41b69
John Chavez <johnchavez@Johns-MBP-2.lan>
2022-07-03 14:39:23 -0400

    adding footer and sidebar.

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useRef, useState } from 'react';
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);  
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    // if video is playing stop
+    if (playing) {
+        videoRef.current.pause();
+        setPlaying(false);
+    }
+    else {
+        videoRef.current.play();
+        setPlaying(true);
+    }
+
+
+    // otherwise if not playing then play
+
+
+  };
+
   return (
     <div className="video">
         <video 
+        onClick={handleVideoPress}
         className='video__player'
         loop
+        ref={videoRef}
         src="https://v16m-default.tiktokcdn-us.com/b669c69aafa15c785220c207c54c26b2/62c22c15/video/tos/useast5/tos-useast5-ve-0068c002-tx/3863576d57624689a1fc5c9388365a91/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2880&bt=1440&cs=0&ds=3&ft=eXWR6H-qMyq8ZvdZ6we2NmE~fl7Gb&mime_type=video_mp4&qs=0&rc=NTc4aGk7OjVpNmRoNmgzOkBpM3FlbGY6Zms7PDMzZzczNEBjMTFiNTEuNi0xLzVeXzJhYSMxNGNpcjRnbWhgLS1kMS9zcw%3D%3D&l=20220703175345010002045007735002021084F99F6">
 
         </video>
---
+++ tiktok/src/VideoFooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './VideoFooter.css';
+
+function VideoFooter() {
+  return (
+    <div className='videoFooter'></div>
+  )
+}
+
+export default VideoFooter
